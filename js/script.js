cd9082ca1b145ea0c7581d39c4dc14162089a019
Martin Ruediger <martinruediger2010@gmail.com>
2018-10-27 22:12:47 -0300

    Changed the js file and add jQuery

--- js/script.js
+++ js/script.js
@@ -1,41 +1,32 @@
-var width = 0;
-var timer = 0;
+timer = 0;
+width = 0;
+bar = $(".progress-bar")[0];
+value = $(".value")[0];
 
-var progressBarLib = (function () {
-  
-  function move() {
-    if(timer == 0) chargerBar();
-    else return false;
-  }
-  
+function fazTudo() {
+
+  if (timer == 0) chargerBar();
+  else return false;
+
   function chargerBar() {
     timer = setTimeout(chargerBar, 50);
-    
-    var bar = document.getElementsByClassName("progress-bar")[0];
-    var value = document.getElementsByClassName("value")[0];
-    
-    if(width < 100) {
-      width++; 
+
+    if (width < 100) {
+      width++;
       bar.style.width = width + '%';
       value.innerHTML = width + "%";
     }
   }
-  
-  function reset() {
-    var bar = document.getElementsByClassName("progress-bar")[0];
-    var value = document.getElementsByClassName("value")[0];
-    
-    window.clearTimeout(timer);
-    timer = 0;
-        
-    bar.style.width = "0%";
-    width = 0;
-    value.innerHTML = "0%";
-  }
-  
-  return {
-    move: move,
-    reset: reset,
-    chargerBar: chargerBar
-  }
-})();
+}
+
+function reset() {  
+  window.clearTimeout(timer);
+  timer = 0;
+
+  bar.style.width = "0%";
+  width = 0;
+  value.innerHTML = "0%";
+}
+
+
+
